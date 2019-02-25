00565c310bdf8cf301c0f14dbec0398fc3c984d0
Roman Muratov <myratov.r@gmail.com>
2019-02-25 16:33:29 +0200

    fixed arrows for case-slider

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -1,4 +1,7 @@
 import slick from 'slick-carousel';
+import { buildIcon } from '../utils';
+
+const icon = buildIcon('arrow-long');
 
 $('.js-cases-slider').slick({
   infinite: true,
@@ -6,8 +9,34 @@ $('.js-cases-slider').slick({
   slidesToScroll: 1,
   dots: true,
   adaptiveHeight: true,
-  prevArrow: $('.js-slider-arrow-prev'),
-  nextArrow: $('.js-slider-arrow-next')
-  // prevArrow: $('.js-slider-arrow-prev'),
-  // nextArrow: $('.js-slider-arrow-next'),
+  prevArrow: `<button class="arrow-prev js-slider-arrow-prev" type="button">
+                <span class="arrow-more">
+                    <span class="arrow-more__icon">
+                        <span class="arrow-more__icon-inner">
+                            <span class="arrow-more__icon-wrap">
+                                ${icon}
+                            </span>
+                        </span>
+                    </span>
+                </span>
+            </button>`,
+  nextArrow: `<button class="arrow-next js-slider-arrow-next" type="button">
+                <span class="arrow-more">
+                    <span class="arrow-more__icon">
+                        <span class="arrow-more__icon-inner">
+                            <span class="arrow-more__icon-wrap">
+                                ${icon}
+                            </span>
+                        </span>
+                    </span>
+                </span>
+            </button>`,
+  responsive: [
+    {
+      breakpoint: 599,
+      settings: {
+        arrows: false
+      }
+    }
+  ]
 });

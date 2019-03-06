4327920375d3d52bc6ba6af3b2738014f6b0d839
Валерий <kaktuz34@gmail.com>
2019-03-06 12:32:03 +0200

    update checkout

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
 import svg4everybody from 'svg4everybody';
 import './common';
 import { BODY, NO_TOUCH } from './constants';
-import { isTouch } from './utils';
+import { isTouch, SCROLL_TO } from './utils';
 svg4everybody();
-
+const scrollToBtn = $('.js-scroll-to');
+// SCROLL_TO
+scrollToBtn.on('click', function() {
+  const position = $($(this).attr('href')).offset().top;
+  SCROLL_TO(position);
+});
 if (!isTouch()) BODY.addClass(NO_TOUCH);
